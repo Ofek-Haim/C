de5efba9ad74e1abb69c1e5b56aa5d5ea656156a
Ofek Haim <ofekhaim333@gmail.com>
2018-07-28 15:43:13 +0300

    comments

---
+++ comments.c
@@ -0,0 +1,19 @@
+// Ofek Haim - 2018
+
+#include "stdafx.h"
+
+void comments() /* This is a comment. */
+/* This is a comment, but
+it is not limited to a single line. It spans
+multiple lines. */
+{
+	// This is a new-style single line comment by C99.
+	printf("comments\n");
+}
+
+int main()
+{
+	comments();
+    return 0; // OK
+}
+
